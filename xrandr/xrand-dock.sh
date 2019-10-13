0d215b92025f33772d4094df040bc319492402da
wuseman <26827453+wuseman@users.noreply.github.com>
2019-10-13 22:58:43 +0200

    Update xrand-dock.sh

--- xrandr/xrand-dock.sh
+++ xrandr/xrand-dock.sh
@@ -1,21 +1,56 @@
 #!/bin/bash
-# Author: wuseman <wuseman@nr1.nu>
+################################################################################
+################################################################################
+####                                                                       #####
+#### A notice to all nerds.                                                #####
+#### If you will copy developers real work it will not make you a hacker.  #####
+#### Resepect all developers, we doing this because it's fun!              #####
+####                                                                       #####
+################################################################################
+################################ SOURCE CODE ###################################
+################################################################################
+####                                                                       #####
+####  Copyright (C) 2018-2019, wuseman                                     #####
+####                                                                       #####
+####  This program is free software; you can redistribute it and/or modify #####
+####  it under the terms of the GNU General Public License as published by #####
+####  the Free Software Foundation; either version 2 of the License, or    #####
+####  (at your option) any later version.                                  #####
+####                                                                       #####
+####  This program is distributed in the hope that it will be useful,      #####
+####  but WITHOUT ANY WARRANTY; without even the implied warranty of       #####
+####  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #####
+####  GNU General Public License for more details.                         #####
+####                                                                       #####
+####  You must obey the GNU General Public License. If you will modify     #####
+####  this file(s), you may extend this exception to your version          #####
+####  of the file(s), but you are not obligated to do so.  If you do not   #####
+####  wish to do so, delete this exception statement from your version.    #####
+####  If you delete this exception statement from all source files in the  #####
+####  program, then also delete it here.                                   #####
+####                                                                       #####
+####  Contact:                                                             #####
+####          IRC: Freenode @ wuseman                                      #####
+####          Mail: wuseman <wuseman@nr1.nu>                               #####
+####                                                                       #####
+################################################################################
+################################################################################
 
-LAPTOP="eDP-1"
-TCL="DP-2-1"
-ACER="DP-2-2"
-DELL="DP-2-3"
-
-# If Docked
+# ARE WE DOCKED
 DOCKED="$(awk '/1/{print $0}' /sys/devices/platform/dock.0/docked)"
-if [[ $DOCKED = 0 ]]; then 
-     xrandr --output $LAPTOP --off
-     xrandr --output $TCL --left-of $ACER
-     xrandr --output $ACER --right-of $TCL
-     xrandr --output $TCL --right-of $LENOVO
-     xrandr --output $LENOVO --left-of $TCL
-     xrandr --output $TCL --mode 1680x1050
-     exit 0
-else
-    xrandr --outut $LAPTOP --auto
+if [[ $DOCKED -eq "1" ]]; then
+
+# SCREENS
+    LAPTOP="eDP-1"
+ACER="DP-2-3"
+LENOVO="DP-2-2"
+TCL="DP-2-1"
+
+    xrandr --output $TCL --right-of $LENOVO
+    xrandr --output $TCL --left-of $ACER
+    xrandr --output $LENOVO --left-of $TCL
+    xrandr --output $LAPTOP --off
+    xrandr --output $ACER --rotate right
+else 
+    xrandr --output $LAPTOP --auto
 fi
